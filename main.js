365f9a49e809f9b049504c4b29064e76d161b60d
rallerG <raller799@live.dk>
2017-04-27 12:13:29 +0200

    Fået input til at virke, samt tælle antal indtastet sider

--- main.js
+++ main.js
@@ -1,19 +1,68 @@
-function visResultat() {
-    var input_A = document.getElementById("vinkel_a")
-    var input_a = document.getElementById("side_a")
-    var input_ha = document.getElementById("højde_a")
-    var input_B = document.getElementById("vinkel_b")
-    var input_b = document.getElementById("side_b")
-    var input_hb = document.getElementById("højde_b")
-    var input_C = document.getElementById("vinkel_c")
-    var input_c = document.getElementById("side_c")
-    var input_hc = document.getElementById("højde_c")
+//Mangler udregning
+    var A = 0;
+    var a = 0;
+    var ha = 0;
+    var B = 0;
+    var b = 0;
+    var hb = 0;
+    var C = 0;
+    var c = 0;
+    var hc = 0;
+    var areal = 0;
+    var omkreds = 0;
+
+
+
+function indsaetResultat(e) {
+    antalSider = 0; //Angiv antal sider som brugeren har indtastet
+    e.preventDefault(); //Forhindre at siden reloader
+    input_A = parseFloat(document.getElementById("vinkel_a").value);
+    input_a = parseFloat(document.getElementById("side_a").value);
+    input_B = parseFloat(document.getElementById("vinkel_b").value);
+    input_b = parseFloat(document.getElementById("side_b").value);
+    input_C = parseFloat(document.getElementById("vinkel_c").value);
+    input_c = parseFloat(document.getElementById("side_c").value);
+
+
+    //Mangler at validerer at brugeren har indtastet minumim 3 felter, hvor min 1 er en side
+
+    //Her tjekker vi om inputet har værdien ikke et NaN, (altså om det er et nummer)
+    if (!isNaN(input_A)) {
+    A = input_A;
+    }
+    if (!isNaN(input_a)) {
+    a = input_a;
+    antalSider ++;
+    }
+    if (!isNaN(input_B)) {
+    B = input_B;
+    }
+    if (!isNaN(input_b)) {
+    b = input_b;
+    antalSider ++;
+    }
+    if (!isNaN(input_C)) {
+    C = input_C;
+    }
+    if (!isNaN(input_c)) {
+    c = input_c;
+    antalSider ++;
+    }
+
+
 };
 
 
 
-var antalsider = 0; //Angiv antal sider som brugeren har indtastet
+document.addEventListener('submit', indsaetResultat);   //Kører funktionen når der trykkes på submit
 
+
+//Alt herover virker
+//------------------------------------------------------------------------------
+
+
+
+// Her vælger vi hvilken regnemetode vi skal anvende, baseret på antallet af sider, som brugeren indtastede
 function udregn() {
     switch (antalSider) {
         case 3:
@@ -29,51 +78,57 @@ function udregn() {
 
 
 
-function udregnTreVinkler(){
+function udregnTreVinkler() {
+    A = acos((b^2+c^2-a^2)/2*b*c);
+    B = acos((a^2+c^2-b^2)/2*b*c);
+    C = acos((a^2+b^2-c^2)/2*b*c);
+};
+function udregnToVinkler() {    //Mangler at tjekke om vi har de nødvendige sider til at udregne
+    if (A > 0) {
+        B = acos((a^2+c^2-b^2)/2*b*c);
+        C = acos((a^2+b^2-c^2)/2*b*c);
+    } else if (B > 0) {
+        A = acos((b^2+c^2-a^2)/2*b*c);
+        C = acos((a^2+b^2-c^2)/2*b*c);
+    } else {
+        A = acos((b^2+c^2-a^2)/2*b*c);
+        B = acos((a^2+c^2-b^2)/2*b*c);
+    }
+};
+function udregnToSider() {
     // Indsæt udregninger
 };
-function udregnToVinkler(){
+function udregnHøjde() {
     // Indsæt udregninger
 };
-function udregnToSider(){
-    // Indsæt udregninger
+
+function udregnAreal() {
+    areal = (a * ha) / 2;
 };
-function udregnAreal(){
-    var areal = (a * ha) / 2;
-};
-function udregnOmkreds(){
-    var omkreds = a + b + c;
+function udregnOmkreds() {
+    omkreds = a + b + c;
 };
 
 
-//Mangler udregning
-    var A = 0;
-    var a = 0;
-    var ha = 0;
-    var B = 0;
-    var b = 0;
-    var hb = 0;
-    var C = 0;
-    var c = 0;
-    var hc = 0;
-    var areal = 0;
-    var omkreds = 0;
+
 
 // Indsæt alle resultaterne på siden
+
 function visResultat() {
-    document.getElementById("vinkel_a_resultat").innerHTML = input_A;
-    document.getElementById("side_a_resultat").innerHTML = input_a;
-    document.getElementById("højde_a_resultat").innerHTML = input_ha;
-    document.getElementById("vinkel_b_resultat").innerHTML = input_B;
-    document.getElementById("side_b_resultat").innerHTML = input_b;
-    document.getElementById("højde_b_resultat").innerHTML = input_hb;
-    document.getElementById("vinkel_c_resultat").innerHTML = input_C;
-    document.getElementById("side_c_resultat").innerHTML = input_c;
-    document.getElementById("højde_c_resultat").innerHTML = input_hc;
+    document.getElementById("vinkel_a_resultat").innerHTML = A;
+    document.getElementById("side_a_resultat").innerHTML = a;
+    document.getElementById("højde_a_resultat").innerHTML = ha;
+    document.getElementById("vinkel_b_resultat").innerHTML = B;
+    document.getElementById("side_b_resultat").innerHTML = b;
+    document.getElementById("højde_b_resultat").innerHTML = hb;
+    document.getElementById("vinkel_c_resultat").innerHTML = C;
+    document.getElementById("side_c_resultat").innerHTML = c;
+    document.getElementById("højde_c_resultat").innerHTML = hc;
     document.getElementById("areal_resultat").innerHTML = areal;
     document.getElementById("omkreds_resultat").innerHTML = omkreds;
 };
 
+//Mangler at tjekke om det virker
 function nustil() {
     A = 0;
     a = 0;
